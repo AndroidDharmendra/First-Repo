1427a35007c9d4f2d23af86a61bd1b5afd476d23
Dharmendra <dharmendrakumar@globussoft.in>
2021-08-21 12:21:52 +0530

    commits under dharmendra repo

---
+++ method.java
@@ -0,0 +1,10 @@
+ class LearnMethod {
+    public static void main(String [] args){
+         add();
+    }
+    public static void add(){
+        int c = 10+20;
+        System.out.println("Add a and b "+c);
+    }
+    
+}
