354981367531c1800b2dcba86582e472ea034fa3
Dharmendra <dharmendrakumar@globussoft.in>
2021-08-21 12:38:09 +0530

    sub done

--- method.java
+++ method.java
@@ -5,6 +5,9 @@
     public static void add(){
         int c = 10+20;
         System.out.println("Add a and b "+c);
+        // Subtract two number
+        int b = 30-3;
+        System.out.println("Subtraction of two number "+b);
     }
     
 }
