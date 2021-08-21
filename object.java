e2bcf7b17f4770ace599b299a52b300b6105730e
Dharmendra <dharmendrakumar@globussoft.in>
2021-08-21 13:57:48 +0530

    add obj.java

---
+++ object.java
@@ -0,0 +1,12 @@
+ class Student {
+    
+    public void studentName(){
+        System.out.println("Dharmendra___");
+    }
+}
+class Launch{
+    public static void main(String[] args){
+        Student s = new Student();
+        s.studentName();
+    }
+}
